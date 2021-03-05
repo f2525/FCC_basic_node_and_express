0f52a50538e48e45580c83dd4ecd90bb59aba113
f2525 <62139970+f2525@users.noreply.github.com>
2021-03-05 20:24:36 +0700

    serve an html file to path /

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+
 app.get("/", (req, res)=>{
-    res.send("Hello Express")
+    absolutePath = __dirname + "/views/index.html";
+    res.sendFile(absolutePath);
 })
 
 
