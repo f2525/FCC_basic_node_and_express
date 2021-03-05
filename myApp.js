19e46f2540b74b5fe391c1b85935af7f55c6fb00
f2525 <62139970+f2525@users.noreply.github.com>
2021-03-05 21:36:19 +0700

    add static assets to myApp

--- myApp.js
+++ myApp.js
@@ -2,14 +2,13 @@ var express = require('express');
 var app = express();
 
 
+
 app.get("/", (req, res)=>{
     absolutePath = __dirname + "/views/index.html";
     res.sendFile(absolutePath);
 })
-
-
-
-
+app.use(express.static(__dirname+"/public"));
+app.use("/public", express.static(__dirname+"/public"))
 
 
 
