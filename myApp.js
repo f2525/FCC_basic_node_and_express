873fda97784f72fa24660daaf20e3d53419d92d2
f2525 <62139970+f2525@users.noreply.github.com>
2021-03-05 19:58:20 +0700

    correction for the get method on / path

--- myApp.js
+++ myApp.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get("/", log)
-function log(){
-    console.log("Hello Express")
-}
+app.get("/", (req, res)=>{
+    res.send("Hello Express")
+})
 
 
 
