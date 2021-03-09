33f06029e40731846e37927e1e29e7b01155c9a3
f2525 <62139970+f2525@users.noreply.github.com>
2021-03-09 23:54:14 +0700

    add middleware logger

--- myApp.js
+++ myApp.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 require("dotenv").config()
 
+app.use(function(req, res, next){
+    console.log(req.method+" "+req.path+" - "+req.ip)
+    next();
+})
 
 app.get("/", (req, res)=>{
     absolutePath = __dirname + "/views/index.html";
